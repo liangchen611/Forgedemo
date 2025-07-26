f55dda0664b3a23908f1a83a01c53b18ef18ac02
liangchen611 <fihggggg@163.com>
2025-07-26 13:18:53 +0800

    new_control
    a new controller for system

--- Assets/Scripts/Controller/playcontrol_newsys.cs
+++ Assets/Scripts/Controller/playcontrol_newsys.cs
@@ -1,16 +1,49 @@
 using UnityEngine;
+using UnityEngine.InputSystem; // 新输入系统的命名空间
 
 public class playcontrol_newsys : MonoBehaviour
 {
+    public float movspeed = 5.0f;
+    private Rigidbody2D rb;
+    private SpriteRenderer sr;
+    private Vector2 moveInput;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        var keyboard = Keyboard.current;
+        float x = 0f;
+        if (keyboard != null)
+        {
+            if (keyboard.aKey.isPressed || keyboard.leftArrowKey.isPressed)
+            {
+                x = -1.0f;
+            }
+            else if (keyboard.sKey.isPressed || keyboard.rightArrowKey.isPressed)
+            {
+                x= 1.0f;
+            }
+
+        }
+
+        moveInput = new Vector2(x, 0);
+
+        if (x != 0)
+            sr.flipX = x < 0;
+    }
+
+    private void FixedUpdate()
+    {
+        if (rb != null)
+        {
+            rb.linearVelocity = new Vector2(moveInput.x * movspeed, rb.linearVelocity.y);
+        }
     }
 }
