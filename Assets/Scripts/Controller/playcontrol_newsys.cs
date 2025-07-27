0666afb973e48e1d5beaeeb3b989e41021bba6ec
liangchen611 <fihggggg@163.com>
2025-07-27 09:30:58 +0800

    Update playcontrol_newsys.cs
    join the logic of jumping(cooling, coyotetime) in new system

--- Assets/Scripts/Controller/playcontrol_newsys.cs
+++ Assets/Scripts/Controller/playcontrol_newsys.cs
@@ -3,10 +3,27 @@ using UnityEngine.InputSystem; // 新输入系统的命名空间
 
 public class playcontrol_newsys : MonoBehaviour
 {
+    // 1st moving
     public float movspeed = 5.0f;
     private Rigidbody2D rb;
     private SpriteRenderer sr;
     private Vector2 moveInput;
+
+    //1st jump
+    public float jumpforce = 10.0f;
+    public LayerMask groundlayer;
+    public Transform groundcheck;
+    public float groundcheckradius = 0.1f;
+    private bool isgrounded;
+
+    //跳跃冷却时间
+    private float lastjumptime = 0f;
+    public float jumpcooldown = 0.1f;
+
+    //检测地面状态
+    private float coyoteTime = 0.1f;
+    private float lastgroundtime = -1f;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -26,9 +43,20 @@ public class playcontrol_newsys : MonoBehaviour
             {
                 x = -1.0f;
             }
-            else if (keyboard.sKey.isPressed || keyboard.rightArrowKey.isPressed)
+            else if (keyboard.dKey.isPressed || keyboard.rightArrowKey.isPressed)
             {
                 x= 1.0f;
+            }
+
+            //jumpcooldown：跃起后，记录当前跃起的时间，随着帧计算当前时间与跃起时间的时间差，如果小于跳跃冷却，则不可起跳
+            //coyotetime：计算跳跃后的时间离最后跃起的时间的时间差，太短说明离起跳时点还很近，防止短时间内连续起跳
+            if (keyboard.spaceKey.wasPressedThisFrame && 
+                isgrounded == true && 
+                Time.time-lastjumptime>jumpcooldown &&
+                Time.time-lastgroundtime<=coyoteTime)
+            {
+                rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpforce);
+                lastjumptime = Time.time;
             }
 
         }
@@ -44,6 +72,23 @@ public class playcontrol_newsys : MonoBehaviour
         if (rb != null)
         {
             rb.linearVelocity = new Vector2(moveInput.x * movspeed, rb.linearVelocity.y);
+        }
+
+        isgrounded = Physics2D.OverlapCircle(groundcheck.position, groundcheckradius, groundlayer);
+        
+        //在平面上，时刻更新最后时间
+        if (isgrounded)
+        {
+            lastgroundtime = Time.time;
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        if (groundcheck != null)
+        {
+            Gizmos.color = Color.green;
+            Gizmos.DrawWireSphere(groundcheck.position, groundcheckradius);
         }
     }
 }
